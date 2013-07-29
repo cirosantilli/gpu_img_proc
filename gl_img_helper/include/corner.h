b2455007b7fdfcf227cfc6bcc499930574fa496f
Prem Sasidharan <prem.sasidharan@gmail.com>
2013-07-29 06:46:20 -0700

    formatting

--- gl_img_helper/include/corner.h
+++ gl_img_helper/include/corner.h
@@ -11,35 +11,35 @@
 class Corner
 {
 public:
-	Corner(int w, int h, QObject* parent);
-	Corner(int w, int h, GLuint& fbid, QObject* parent);
-	~Corner();
+    Corner(int w, int h, QObject* parent);
+    Corner(int w, int h, GLuint& fbid, QObject* parent);
+    ~Corner();
 
 public:	
-	bool init();
-	bool detect(GLuint source, GLuint dest);
-	
+    bool init();
+    bool detect(GLuint source, GLuint dest);
+
     void inc_threshold();
     void dec_threshold();
-	
-	int threshold() const;
-	void set_threshold(int thresh);
-	
-protected:
-	void create_pipe();
+
+    int threshold() const;
+    void set_threshold(int thresh);
 	
 protected:
-	int width;
-	int height;
+    void create_pipe();
+	
+protected:
+    int width;
+    int height;
     bool fb_flag;
-	GLuint fbo_id;
-	GLuint& fb_id;
-	
-	Eigen_shader es;
-	Matrix_m_shader mms;
+    GLuint fbo_id;
+    GLuint& fb_id;
+
+    Eigen_shader es;
+    Matrix_m_shader mms;
     Smooth_m_shader sms;
-	Nms_threshold_shader nts;
-	
+    Nms_threshold_shader nts;
+
     QVector<Abstract_shader*> pipe;
 };
 
